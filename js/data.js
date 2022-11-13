9745b15d6c042825b847bf9efc7628e5dcb28086
Oleg Barbashow <teosofio@gmail.com>
2022-11-13 07:35:06 +0200

    Added a modular structure

---
+++ js/data.js
@@ -0,0 +1,59 @@
+import {getRandomNumber, getRandomArrayValue, getRandomArrayValues, makeUniqueRandomIntegerGenerator} from './util.js';
+
+const SIMILAR_PHOTO_COUNT = 25;
+const MIN_ID = 1;
+const MAX_ID = 25;
+const MIN_URL_NUMBER = 1;
+const MAX_URL_NUMBER = 25;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+
+const getUniqueRandomId = makeUniqueRandomIntegerGenerator(MIN_ID, MAX_ID);
+const getUniqueRandomUrl = makeUniqueRandomIntegerGenerator(MIN_URL_NUMBER, MAX_URL_NUMBER);
+const getUniqueRandomCommentId = makeUniqueRandomIntegerGenerator(1, 1000);
+
+const photoDescriptions = [
+  'good shot',
+  'great view',
+  'my art',
+];
+
+const messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const names = [
+  'Артем',
+  'Иван',
+  'Дима',
+  'Маша',
+  'Катя',
+];
+
+const createComments = () => {
+  return {
+    id: getUniqueRandomCommentId(),
+    avatar: 'img/avatar-' + getRandomNumber(1, 6) + '.svg',
+    message: getRandomArrayValues(messages, 1, 2),
+    name: getRandomArrayValue(names),
+  }
+}
+
+const createPhotoData = () => {
+  return {
+    id: getUniqueRandomId(),
+    url: 'photos/' + getUniqueRandomUrl() +'.jpg',
+    description: getRandomArrayValue(photoDescriptions),
+    likes: getRandomNumber(MIN_LIKES, MAX_LIKES),
+    comments: new Array(getRandomNumber(1, 10)).fill(null).map(() => createComments()),
+  }
+}
+
+let similarPhotos = new Array(SIMILAR_PHOTO_COUNT).fill(null).map(() => createPhotoData());
+
+console.log(similarPhotos);
---
+++ js/util.js
@@ -0,0 +1,42 @@
+const getRandomNumber = function (from, to) {
+  if (from < 0 || to < 0) {
+    return 'Range can only be positive';
+  } else if (to < from || from === to) {
+    return '"to" value must be more then "from" value'
+  }
+  return Math.floor(Math.random() * (to - from + 1)) + from;
+}
+
+const makeUniqueRandomIntegerGenerator = (min, max) => {
+  const previousValues = [];
+
+  return () => {
+    let currentValue = getRandomNumber(min, max);
+
+    if (previousValues.length >= (max - min + 1)) {
+      throw new Error(`Iterate over all numbers from the range from ${min} to ${max}`);
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomNumber(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+const getRandomArrayValue = (array) => {
+  return array[getRandomNumber(0, array.length - 1)];
+};
+
+const getRandomArrayValues = (array, from, to) => {
+  let string = '';
+  let number = getRandomNumber(from, to);
+  while (number > 0) {
+    string += array[getRandomNumber(0, array.length - 1)];
+    number--;
+  }
+
+  return string;
+};
+
+export {getRandomNumber, getRandomArrayValue, getRandomArrayValues, makeUniqueRandomIntegerGenerator};
